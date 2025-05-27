4048e9391c8f606a40805193084282c2ccfcfb07
Roberto <juanrobcode@gmail.com>
2025-05-26 22:00:36 -0700

    last try

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/club/',
   plugins: [react()],
   css: {
     preprocessorOptions: {
